0b8509ed275f32ae13ce19fda843d550c5858410
Philip Koogan <philip.koogan@rockets.utoledo.edu>
2015-03-24 23:34:47 -0400

    lesson 9 javascript commit

---
+++ js/myFunctions.js
@@ -0,0 +1,10 @@
+function toggle(element){
+	var e = document.getElementById(element);
+	if(e.style.display == "block"){
+		e.style.display = "none";
+	}
+		else {
+			e.style.display = "block";
+		}
+			
+};
